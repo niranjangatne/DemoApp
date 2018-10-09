bbbf2c440f68dc9fffebc261007f0da6277c4953
niranjan.gatne <abhigatne@gmail.com>
2018-10-09 17:30:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+app.set('viewengine','ejs');
+
+
+app.use(express.static(__dirname+'/public'));
+
+// app.use((req,res,next) => {
+// res.render('maintenance.ejs');
+// });
+
+app.use((req,res,next) => {
+let log = new Date().toString() + req.method + res.path + '\n';
+console.log(log);
+fs.appendFile('server.log',log);
+next();
+});
+
+app.get('/',(req,res) => {
+res.send('<h1>Hellooo Express!!</h1>');
+});
+
+app.get('/User',(req,res) => {
+    // res.send(
+    //     {
+    //     'name': 'FIGmd',
+    //     'address':'Nanded City'
+    //     }
+    // );
+
+    res.render('home.ejs', {
+        pagetitle: 'ExpressApp',
+        username: 'Niranjan'
+    })
+    });
+
+    
+app.listen(3000,() => {
+console.log(`server started on port 3000`);
+});
